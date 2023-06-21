85d70d1346518f01e78642e819c9dcdad335f60e
temeculous <zacerious@gmail.com>
2023-06-20 23:47:23 -0400

    Final commit

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,6 +1,11 @@
 const Flight = require("../models/Flight");
 
-module.exports = { index, getNew, addNew };
+module.exports = { index, getNew, addNew, getAllFlights };
+
+async function getAllFlights(req, res) {
+  const flights = await Flight.find();
+  return flights;
+}
 
 async function index(req, res) {
   const flights = await Flight.find();
@@ -11,15 +16,11 @@ function getNew(req, res) {
   res.render("flights/new");
 }
 
-
 async function addNew(req, res) {
-    for (let key in req.body) {
-        if (req.body[key] === '') delete req.body[key];
-    }
-    try {
-        await Flight.create(req.body);
-        res.redirect('/')
-    } catch (err) {
-        res.render("flights/new", { errMsg: err });
-    }
+  try {
+    await Flight.create(req.body);
+    res.redirect("/");
+  } catch (err) {
+    res.render("flights/new", { errMsg: err });
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var flightsCtrl = require("../controllers/flights");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Flights' });
+router.get("/", async function (req, res, next) {
+  var flights = await flightsCtrl.getAllFlights();
+  res.render("index", { title: "Flights", flights });
 });
 
 module.exports = router;
